6e73e4d2ebe3f5737b2bd6a1df2ed7c5b7deedeb
mattbarcelona <mathieu.driencourt@yahoo.fr>
2025-04-13 19:52:31 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import './App.css';
+// App.js
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Calculator from "./Calculator";
+import Result from "./Result";
+import { CalculatorProvider } from "./CalculatorProvider";
+import "./App.css";
 
 function App() {
   return (
-    <></>
+    <CalculatorProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Calculator />} />
+          <Route path="/resultado" element={<Result />} />
+        </Routes>
+      </Router>
+    </CalculatorProvider>
   );
 }
 
